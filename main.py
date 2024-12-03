6d1d97715493b594a9a8cfde12532cffb5c2e95c
Kengam <61079298+Kengam@users.noreply.github.com>
2024-12-04 06:32:42 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-import lightgbm as lgb
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.tree
+import lightgbm
+from lightgbm import LGBMClassifier
 import optuna
 import matplotlib.pyplot as plt
 import sys
@@ -26,37 +29,69 @@ def evaluate(y_true, y_pred):
 
 def lgb_classifier(x_train, y_train):
     def objective(trial):
-        train_x, valid_x, train_y, valid_y = train_test_split(x_train, y_train, test_size=test_size)
-        dtrain = lgb.Dataset(train_x, label=train_y)
-        param = {
+        train_x, valid_x, train_y, valid_y = train_test_split(x_train, y_train, test_size=test_size, random_state=seed)
+        params = {
             'objective': 'binary',
             'metric': 'binary_logloss',
             'verbosity': -1,
             'boosting_type': 'gbdt',
             'class_weight': 'balanced',
+            'n_estimators': trial.suggest_int('n_estimators', 10, 100),
             'lambda_l1': trial.suggest_float('lambda_l1', 1e-8, 10.0, log=True),
             'lambda_l2': trial.suggest_float('lambda_l2', 1e-8, 10.0, log=True),
             'num_leaves': trial.suggest_int('num_leaves', 2, 256),
             'feature_fraction': trial.suggest_float('feature_fraction', 0.4, 1.0),
             'bagging_fraction': trial.suggest_float('bagging_fraction', 0.4, 1.0),
             'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
-            'min_child_samples': trial.suggest_int('min_child_samples', 5, 100)
+            'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
+            'seed': seed
         }
-        clf = lgb.train(param, dtrain)
+        clf = LGBMClassifier(**params)
+        clf.fit(train_x, train_y)
         pred_y = clf.predict(valid_x)
         score = evaluate(valid_y, pred_y)
         return score
         
     study = optuna.create_study(direction='maximize')
     study.optimize(objective, n_trials=n_trials)
-    dtrain = lgb.Dataset(x_train, label=y_train)
-    clf = lgb.train(study.best_trial.params, dtrain)
+    clf = LGBMClassifier(**study.best_trial.params)
+    clf.fit(x_train, y_train)
     return clf
 
 def lgb_plot(clf):
     for i in range(10):
-        lgb.plot_tree(clf, tree_index=i)
-        plt.savefig(output_path + f'tree_{i}.png')
+        lightgbm.plot_tree(clf, tree_index=i)
+        plt.savefig(output_path + f'lgb_tree_{i}.png')
+        plt.clf()
+        plt.close()
+
+def rf_classifier(x_train, y_train):
+    def objective(trial):
+        train_x, valid_x, train_y, valid_y = train_test_split(x_train, y_train, test_size=test_size, random_state=seed)
+        params = {
+            'n_estimators': trial.suggest_int('n_estimators', 10, 100),
+            'criterion': 'gini',
+            'max_depth': trial.suggest_int('max_depth', 2, 32, log=True),
+            'min_samples_split': trial.suggest_int('min_samples_split', 2, 100),
+            'random_state': seed,
+            'class_weight': 'balanced',
+        }
+        clf = RandomForestClassifier(**params)
+        clf.fit(train_x, train_y)
+        pred_y = clf.predict(valid_x)
+        score = evaluate(valid_y, pred_y)
+        return score
+
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective, n_trials=n_trials)
+    clf = RandomForestClassifier(**study.best_trial.params)
+    clf.fit(x_train, y_train)
+    return clf
+
+def rf_plot(clf):
+    for i in range(10):
+        sklearn.tree.plot_tree(clf.estimators_[i])
+        plt.savefig(output_path + f'rf_tree_{i}.png')
         plt.clf()
         plt.close()
 
@@ -76,11 +111,12 @@ def main():
     )
     # toyデータ作成終了
     plot_histogram([df[df['y']==0]['sepal length (cm)'].values, df[df['y']==1]['sepal length (cm)'].values],'title',label_list=['a','b'])
-    clf = lgb_classifier(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    lgb_plot(clf)
-    print(f'{metrics.roc_auc_score(y_test, y_pred)}, {metrics.average_precision_score(y_test, y_pred)}')
-    plot_shap(clf, df.drop('y', axis=1))
+    for classifier, plot in [(lgb_classifier, lgb_plot), (rf_classifier, rf_plot)]:
+        clf = classifier(x_train, y_train)
+        y_pred = clf.predict(x_test)
+        print(f'{metrics.roc_auc_score(y_test, y_pred)}, {metrics.average_precision_score(y_test, y_pred)}')
+        plot(clf)
+        #plot_shap(clf, df.drop('y', axis=1))
 
 if __name__ == '__main__':
     main()
